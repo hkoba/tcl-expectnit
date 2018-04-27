540aa24a638995f868b9b3042c9fc0c8e6343bfc
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-04-27 11:47:35 +0900

    To match prompt.

--- ex/ex1.tcl
+++ ex/ex1.tcl
@@ -16,9 +16,11 @@ apply {{argList} {
         }]]
 
     exp send "setopt no_zle no_prompt_cr; PROMPT='%# ' RPROMPT=''\n\n"
+    # => "%\u001b[49m \u001b[K\u001b[67C \u001b[1m~\u001b[0m\u001b[49m\u001b[69D\u001b[?2004hs\u0008setopt no_zle no_prompt_cr; PROMPT='%# ' RPROMPT=''\u001b[?2004l\u001b[K\r\r\n% % "
 
-    exp expect "% "
-    
+    # ↑Match last "% % "
+    exp expect -re {(% \r?\n?)+}
+
     parray [exp myvar $varName]
 
     exp send "pwd\n"
