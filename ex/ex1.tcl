61995d238bbfaf5513e0907f4a70831d3864b911
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-05-27 18:36:55 +0900

    Remove unused code

--- ex/ex1.tcl
+++ ex/ex1.tcl
@@ -14,11 +14,6 @@ apply {{argList} {
 
     exp spawn {*}$argList
     
-    trace add variable [exp myvar $varName] write \
-        [list apply [list args {
-            puts write:$args
-        }]]
-
     exp send "setopt no_zle no_prompt_cr; PROMPT='%# ' RPROMPT=''\n\n"
     # => "%\u001b[49m \u001b[K\u001b[67C \u001b[1m~\u001b[0m\u001b[49m\u001b[69D\u001b[?2004hs\u0008setopt no_zle no_prompt_cr; PROMPT='%# ' RPROMPT=''\u001b[?2004l\u001b[K\r\r\n% % "
 
